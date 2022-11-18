f5355cc15abb0b0363cc9c0a37f9442f9843b084
Mariela Valentini <marielavalentini@outlook.com>
2022-11-18 16:48:50 -0300

    Hospital

--- WebApiFinalValentiniMariela/Controllers/HospitalController.cs
+++ WebApiFinalValentiniMariela/Controllers/HospitalController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,12 +22,21 @@ namespace WebApiFinalValentiniMariela.Controllers
 
         [HttpGet]
 
-        public IEnumerable Get() //trae la lista de hospitales
+        public dynamic Get() //trae la lista de hospitales
         {
-            List<Hospital> hospitales = Context.Hospitales.ToList();
+            dynamic hospitales = (from h in Context.Hospitales
+                                  select new { h.Nombre, h.Telefono, h.Num_Camas}).ToList();
             return hospitales;
 
         }
+        [HttpGet("/api/hospital/{CantidadCamas}")]
 
+        public dynamic Get(int CantidadCamas) //trae la lista de aquellos hospitales donde el numero de camas sea mayor al recibido por parametro
+        {
+            dynamic hospitales = (from h in Context.Hospitales
+                                  where h.Num_Camas > CantidadCamas
+                                  select new { h.Nombre, h.Telefono, h.Num_Camas }).ToList();
+            return hospitales;
+        }
     }
 }
